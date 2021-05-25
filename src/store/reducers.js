b47894adea0766182ee258b94e7d74a6c1a76368
jaimeperezortega <jaimeperezortegadev@gmail.com>
2021-05-25 17:28:04 +0200

    Troceamos el reducer que maneja el estado de auth + adverts en 2 reducers que cada uno maneja su parte del estado

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -9,26 +9,66 @@ const initialState = {
 
 // Redux permite combinar distintos reducers en uno solo de tal forma que cada reducer maneje una perte concreta del estado. En Este caso, tenemos un reducer que maneja solo el estado auth a true o false pero cuando ncesirtemos manejar más estados, tendremos que crear más reducers.
 
-function reducer(state = initialState, action) {
+// function oldReducer(state = initialState, action) {
+//     switch (action.type) {
+//         case AUTH_LOGIN:
+//             return  {...state, auth:true};
+
+//         case AUTH_LOGOUT:
+//             return {...state, auth:false};
+
+//         case ADVERTS_LOADED:
+//             return {...state, adverts: action.payload.adverts};
+        
+//         case ADVERTS_CREATED:
+//             return {...state, adverts: [...state.adverts, action.payload.advert]}
+//             //return {...state, adverts: state.adverts.concat(action.payload.advert)}
+            
+//         default:
+//             return state;
+            
+//     }
+
+// }
+
+function auth(state, action){
     switch (action.type) {
         case AUTH_LOGIN:
-            return  {...state, auth:true};
+            return  true;
 
         case AUTH_LOGOUT:
-            return {...state, auth:false};
-
-        case ADVERTS_LOADED:
-            return {...state, adverts: action.payload.adverts};
-        
-        case ADVERTS_CREATED:
-            return {...state, adverts: [...state.adverts, action.payload.advert]}
-            //return {...state, adverts: state.adverts.concat(action.payload.advert)}
+            return false;
             
         default:
             return state;
             
     }
 
+}
+
+function adverts(state, action) {
+    switch (action.type) {
+
+        case ADVERTS_LOADED:
+            return action.payload.adverts;
+        
+        case ADVERTS_CREATED:
+            return [...state, action.payload.advert];
+            
+        default:
+            return state;
+            
+    }
+
+}
+
+
+
+function reducer(state= initialState, action) {
+    return {
+        auth: auth(state.auth, action) ,
+        adverts: adverts(state.adverts, action),
+    }
 }
 
 export default reducer;
--- src/store/selectors.js
+++ src/store/selectors.js
@@ -2,4 +2,9 @@
 
 export const getIsLogged = state => state.auth;
 
-export const getAdvertsSelector = state => state.adverts;
+export const getAdvertsSelector = state => state.adverts.sort((a1,a2)=> {
+    if(a1.createdAt < a2.createdAt) return 1;
+    return -1 
+});
+
+    // Todo este rollo del sort es para que los anuncios se ordenen del más nuevo al más antiguo. Si lo dejamos así funciona perfectamente: export const getAdvertsSelector = state => state.adverts
