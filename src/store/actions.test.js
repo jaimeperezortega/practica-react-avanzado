569c46e49f1a38fe645a0e96c71badf280ecf695
jaimeperezortega <jaimeperezortegadev@gmail.com>
2021-05-28 17:58:22 +0200

    Añadido test a advertsLoadedSuccess

--- src/store/actions.test.js
+++ src/store/actions.test.js
@@ -1,5 +1,5 @@
-import {authLoginRequest} from './actions';
-import { AUTH_LOGIN_FAILURE, AUTH_LOGIN_REQUEST } from './types';
+import {authLoginRequest, advertsLoadedSuccess} from './actions';
+import { AUTH_LOGIN_REQUEST, ADVERTS_LOADED_SUCCESS } from './types';
 
 describe('authLoginRequest', ()=>{
     test('should return an AUTH_LOGIN_REQUEST action ', () => {
@@ -9,3 +9,12 @@ describe('authLoginRequest', ()=>{
         expect(result).toEqual({type: AUTH_LOGIN_REQUEST}); // ESte es el más preciso de los 3. Si metemos un nuevo parámetro en el objeto del action, este test no pasaría.
     })
 });
+
+describe('advertsLoadedSuccess', () => {
+    test('should return an ADVERTS_LOADED_SUCCESS action', () => {
+        const adverts = 'adverts';// Nos tenemos que crear unos adverts que no tiene por qué ser un array de datos. Al ser un test unitario que no va a pasar por un reducer o algo así, lo único que nos interesa es que nos devuelva este mismo dato. Lo único que te importa es que lo que tú le pasas te lo devuelva en el payload
+        const expectedAction = {type: ADVERTS_LOADED_SUCCESS, payload: adverts};
+        const result = advertsLoadedSuccess(adverts);
+        expect(result).toEqual(expectedAction);
+    })
+})
